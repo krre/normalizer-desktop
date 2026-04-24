7f99aeeca38f1e858a11374c46477835d2a8a95d
Vladimir Zarypov <krre31@gmail.com>
2026-04-24 06:09:30 +0300

    Remove Vulkan support

--- app/main.cpp
+++ app/main.cpp
@@ -1,25 +1,11 @@
 #include <core/Application.h>
 #include <ui/MainWindow.h>
 #include <settings/FileSettings.h>
-#include <QLoggingCategory>
-#include <QVulkanInstance>
 
 int main(int argc, char* argv[]) {
     Application app(argc, argv);
 
     FileSettings settings;
-
-    if (settings.loggingVulkan()) {
-        QLoggingCategory::setFilterRules("qt.vulkan=true");
-    }
-
-    QVulkanInstance instance;
-    instance.setLayers({ "VK_LAYER_KHRONOS_validation" });
-    instance.setApiVersion(QVersionNumber(1, 4));
-
-    if (!instance.create()) {
-        qFatal("Failed to create Vulkan instance: %d", instance.errorCode());
-    }
 
     MainWindow mainWindow(&settings);
     mainWindow.show();
